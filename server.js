9cda5e69baccab751cebe61b23a2f76bfc43d3ae
Rob Conery <rob@wekeroad.com>
2013-03-11 11:31:39 -1000

    Initial load

---
+++ server.js
@@ -0,0 +1,34 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.engine('html', require('ejs').renderFile);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+var congoServer = require("./lib/mongoapi_server")(app);
+app.get('/', routes.index);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
