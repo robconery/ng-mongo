262eb9b3a50da5222ab5ff2ac397bb2d89d99ae3
Rob Conery <rob@wekeroad.com>
2013-03-18 08:32:54 -1000

    Episode 2

--- public/javascripts/ng-mongo/app.js
+++ public/javascripts/ng-mongo/app.js
@@ -1,7 +1,29 @@
-var ListCtrl = function($scope, $http){
+var ngMongo = angular.module("ngMongo",['ngResource']);
+
+
+ngMongo.factory("Mongo",function($resource){
+  return {
+    database : $resource("/mongo-api/dbs")
+  }
+});
+
+ngMongo.controller("ListCtrl", function($scope, Mongo){
   //http get returns a promise...
-  var result = $http.get("/mongo-api/dbs");
-  result.success(function(data){
-    $scope.items = data;
-  });
-}
+  $scope.items = Mongo.database.query();
+  $scope.addDb = function(){
+    var dbName = $scope.newDbName;
+    if(dbName){
+      var newDb = new Mongo.database({name : dbName});
+      newDb.$save();
+      $scope.items.push(newDb);
+    }
+  }
+  $scope.removeDb = function(db){
+    if(confirm("Delete this database? There's no undo...")){
+      db.$delete({name : "name"});
+      $scope.items.splice($scope.items.indexOf(db),1)
+    }
+  }
+
+
+});
